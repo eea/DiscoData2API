9277b29ac0d9ae74f510c6e17d2a20f8697907f4
Oskar Esparza <oesparza@bilbomatica.es>
2025-01-07 16:01:40 +0100

    Extract fields from custom view

---
+++ back/private/Model/CreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DiscoData2API_Priv.Model
+{
+    public class CreateViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Query { get; set; } 
+        public string? User { get; set; }
+        public string? Version { get; set; }
+
+
+    }
+}
--- back/private/Model/MongoDocument.cs
+++ back/private/Model/MongoDocument.cs
@@ -2,7 +2,8 @@ namespace DiscoData2API_Priv.Model;
 
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-
+using System.Diagnostics.Eventing.Reader;
+using ZstdSharp.Unsafe;
 
 [BsonIgnoreExtraElements]
 public class MongoDocument : MongoBaseDocument
@@ -21,7 +22,7 @@ public class MongoDocument : MongoBaseDocument
     public string Description { get; set; } = null!;
 
     [BsonElement("userAdded")]
-    public string UserAdded { get; set; } = null!;
+    public string? UserAdded { get; set; } = null!;
 
     [BsonElement("isActive")]
     public Boolean IsActive { get; set; }
@@ -41,15 +42,59 @@ public class MongoDocument : MongoBaseDocument
 
 public class Field
 {
+
+    private string _type= string.Empty;
+
      [BsonElement("name")]
      public string Name { get; set; } = null!;
 
     [BsonElement("type")]
-    public string Type { get; set; } = null!;
+    public string Type
+    {
+        get {  return _type; }
+        set  {
+            _type = "string";
+            switch (value)
+            {
+                case "CHARACTER":
+                case "CHARACTER VARYING": 
+                    _type = "string";
+                    break;
+                case "INTEGER":
+                    _type= "int32";
+                    break;
+                case "BIGINT":
+                    _type = "int64";
+                    break;
+                case "DOUBLE":
+                case "FLOAT":
+                case "FLOAT32":
+                case "FLOAT64":
+                    _type = "float64";
+                    break;
+                case "geometry":
+                    _type = "geometry";
+                    break;
+                case "BINARY":
+                case "BINARY VARYING":
+                    _type = "binary";
+                    break;
+
+                default:
+                    _type = value.ToLower();
+                    break;
+            }
+        }
+    }   
 
     [BsonElement("isNullable")]
-    public bool IsNullable { get; set; }
+    public bool IsNullable { get; set; } = true!;
 
     [BsonElement("columnSize")]
     public string? ColumnSize { get; set; }
+
+    [BsonIgnoreIfNull]
+    [BsonElement("description")]
+    public string Description { get; set; } = null!;
+
 }
