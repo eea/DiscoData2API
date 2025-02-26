fb710dc7d1129d1136c63aec9e4fdec156be9b5b
Oskar Esparza <oesparza@bilbomatica.es>
2025-02-26 09:26:02 +0100

    Add prometheus metrics and compression

--- back/private/Program.cs
+++ back/private/Program.cs
@@ -56,18 +56,11 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
-
 app.UseSwagger();
 //app.UseSwaggerUI();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-
-
 });
 app.UseResponseCompression();
 app.UseHttpsRedirection();
